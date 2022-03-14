9855f02d08dbbc0eebed26715d6c3f4835cf96e1
Anish M Prasad <Anish.m.prasad@gmail.com>
2022-03-15 00:41:06 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-restricted-globals */
+// https://web.dev/offline-fallback-page/
+// @ts-nocheck
+
+const CACHE_STATIC_NAME = 'assign-static-v2-%BUILDVERSION%';
+const CACHE_DYNAMIC_NAME = 'assign-dynamic-v2-%BUILDVERSION%';
+
+const whiteList = ['/assign', '/assign/', '/assign/index.html'];
+
+// The URL constructor is available in all browsers that support SW.
+self.addEventListener('install', function (event) {
+	console.log('[Service Worker] Installing Service Worker ...', event);
+	event.waitUntil(
+		caches.open(CACHE_STATIC_NAME).then(function (cache) {
+			console.log('[Service Worker] Precaching App Shell');
+			cache.addAll(whiteList);
+		})
+	);
+});
+
+self.addEventListener('activate', function (event) {
+	console.log('[Service Worker] Activating Service Worker ....', event);
+	event.waitUntil(
+		caches.keys().then(function (keyList) {
+			return Promise.all(
+				keyList.map(function (key) {
+					if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+						console.log('[Service Worker] Removing old cache.', key);
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
+	return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+	if (event.request.url.indexOf('/assign') === -1) {
+		return false;
+	}
+	// console.log('[Service Worker] cached => ', event.request.url)
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			if (response) {
+				return response;
+			} else {
+				return fetch(event.request)
+					.then(function (res) {
+						return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+							cache.put(event.request.url, res.clone());
+							return res;
+						});
+					})
+					.catch(function (err) {
+						console.log('[Service Worker] Error Occured.', err);
+					});
+			}
+		})
+	);
+});
