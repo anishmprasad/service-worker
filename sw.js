beb2b64bb94c031226bd462e092ee412731f65a7
Anish M Prasad <Anish.m.prasad@gmail.com>
2022-03-22 23:43:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,11 +19,11 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('activate', function (event) {
-	console.log('[Service Worker] Activating Service Worker ....', event);
+	console.log('[Service Worker] Activating Service Worker ....');
 	event.waitUntil(
 		caches.keys().then(function (keyList) {
 			return Promise.all(
-				keyList.map(function (key) {
+				keyList.filter(keyList => keyList.startsWith(SCOPE)).map(function (key) {
 					if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
 						console.log('[Service Worker] Removing old cache.', key);
 						return caches.delete(key);
